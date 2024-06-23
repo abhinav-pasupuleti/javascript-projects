34318f18f30a5fc557e4bce0d106fd0421f0a156
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-06-23 12:20:39 -0500

    Completed arrays studio along with bonus

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -3,12 +3,35 @@ let equipment = "space suits,jet packs,tool belts,thermal detonators";
 let pets = "parrots,cats,moose,alien eggs";
 let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
+
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+food = food.split(',').sort();
+equipment = equipment.split(',').sort();
+pets = pets.split(',').sort();
+sleepAids = sleepAids.split(',').sort();
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [];
+cargoHold.push(food, equipment, pets, sleepAids);
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+const input = require('readline-sync');
+let userInput = input.question("Select a cabinet (0-3) in the cargohold: ");
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+console.log(`The have selected cabinet ${userInput} which contains: ${cargoHold[userInput]}`);
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+let bonusCab = input.question("Select a cabinet (0-3) in the cargohold: ");
+let bonusItem = input.question("Select a particular item you want: ");
+
+//console.log(cargoHold[bonusCab].includes(bonusItem));
+if(cargoHold[bonusCab].includes(bonusItem))
+{
+    console.log(`Cabinet ${bonusCab} DOES contain ${bonusItem}`);
+}
+else
+{
+    console.log(`Cabinet ${bonusCab} DOES NOT contain ${bonusItem}`);
+}
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,25 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let newStr = str.slice(0,3);
+let newStr2 = str.slice(3,10);
+console.log(newStr);
+newStr2 = newStr2.concat(newStr);
+console.log(newStr2);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`The original string is ${str}, the new string is ${newStr2}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let userInput = input.question("Enter the number of letters that will be relocated: ");
+
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (Number(userInput) > str.length){
+    userInput = 3;
+    console.log(`Your input was longer than ${str.length} so now the userInput is 3`);
+}
+newStr = str.slice(0,userInput);
+newStr2 = str.slice(userInput,10);
+newStr2 = newStr2.concat(newStr);
+console.log(newStr2);
